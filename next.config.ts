e4c4b62503323b15c421e08e3cbfb3ffd0e757ae
supersimple33 <40609224+supersimple33@users.noreply.github.com>
2026-09-22 18:54:29 -0700

    feat: redirect for cboe

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/cboe/:path*",
+        destination: "https://cdn.cboe.com/api/global/delayed_quotes/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
